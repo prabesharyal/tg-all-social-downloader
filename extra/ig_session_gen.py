525b517743c03267a198d9dbe76dba1bb82e2fc6
Prabesh Aryal <73214060+prabesharyal@users.noreply.github.com>
2022-09-07 14:26:39 +0545

    no need of this

--- extra/ig_session_gen.py
+++
@@ -1,7 +0,0 @@
-import instaloader
-
-USER = input("Enter Account Username : ")
-PASSWORD = input("Enter Account Passsword : ")
-L = instaloader.Instaloader()
-L.login("{}".format(USER), "{}".format(PASSWORD))   
-L.save_session_to_file(filename = '{}.session'.format(USER))
